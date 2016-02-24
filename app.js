17c9eaf021391266629f74c2b2a60b6415c39a75
Ross Hettel <rosshettel@gmail.com>
2016-02-24 17:39:11 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var Twitter = require('twitter'),
+    twitter = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    }),
+    logger = require('./logger'),
+    // kanyeTwitterID = '259495612'; //actually rosshettel
+    kanyeTwitterID = '169686021';
+
+    twitter.stream('statuses/filter', {follow: kanyeTwitterID}, function (stream) {
+        stream.on('data', function (tweet) {
+            if (tweet.user && tweet.user.id && tweet.user.id.toString() === kanyeTwitterID) {
+                logger.debug('new kanye tweet', tweet);
+
+                var newTweetContent = "Liz Lemon, " + tweet.text;
+                logger.info('Posting new Kanye tweet', newTweetContent);
+
+                twitter.post('statuses/update', {status: newTweetContent}, function (err, tweet, response) {
+                    if (err) {
+                        logger.error('Error posting tweet:', err);
+                    }
+                });
+            }
+        });
+
+        stream.on('error', function (error) {
+            logger.error('Stream error', error);
+        });
+    });
---
+++ logger.js
@@ -0,0 +1,24 @@
+var winston = require('winston'),
+    slackWinston = require('slack-winston'),
+    slackWebhook = process.env.LOGGING_WEBHOOK || 'n/a',
+    logger = new (winston.Logger)({
+        transports: [
+            new winston.transports.Console({
+                level: 'debug',
+                colorize: true,
+                prettyPrint: true,
+                timestamp: function () {
+                    return new Date().toLocaleString();
+                }
+            }),
+            new slackWinston.Slack({
+                domain: 'rosshettel',
+                webhook_url: slackWebhook,
+                channel: 'tracywest-logs',
+                silent: slackWebhook === 'n/a',
+                level: 'debug'
+            })
+        ]
+    });
+
+module.exports = logger;
